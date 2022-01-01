56beae14b387097eaf8d2fdb0c6f27b652fd2baa
Thein Than Hein <thein.than.hein.th@gmail.com>
2022-01-01 06:45:02 -0800

    add library

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # book
+
+Table styling quick tips
+
+Make your table markup as simple as possible, and keep things flexible, e.g. by using percentages, so the design is more responsive.
+Use table-layout: fixed to create a more predictable table layout that allows you to easily set column widths by setting width on their headings (<th>).
+Use border-collapse: collapse to make table elements borders collapse into each other, producing a neater and easier to control look.
+Use <thead>, <tbody>, and <tfoot> to break up your table into logical chunks and provide extra places to apply CSS to, so it is easier to layer styles on top of one another if required.
+Use zebra striping to make alternative rows easier to read.
+Use text-align to line up your <th> and <td> text, to make things neater and easier to follow.
+
+position: absolute;
+left: 50%;
+top: 50%;
+transform: translate(-50%, -50%);
+
+box-shadow: x-axis y-axis blur blur-radius color;
--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+let myLibrary = [
+  { title: "The Hobbit", author: "Tjj", numberOfPages: 453, isRead: "No" },
+  {
+    title: "Lord of the Rings",
+    author: "Toyel",
+    numberOfPages: 234,
+    isRead: "No",
+  },
+];
+
 function Book(title, author, numberOfPages, isRead) {
   this.title = title;
   this.author = author;
@@ -8,5 +18,63 @@ function Book(title, author, numberOfPages, isRead) {
   };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-console.log(theHobbit.info());
+function addBookToLibrary(title, author, numberOfPages, isRead) {
+  const book = new Book(title, author, numberOfPages, isRead);
+  myLibrary.push(book);
+  addABookTableRow(book);
+}
+
+function createABookTableRow(book) {
+  const tableRow = document.createElement("tr");
+  const title = document.createElement("th");
+  const author = document.createElement("td");
+  const numberOfPages = document.createElement("td");
+  const isRead = document.createElement("td");
+  title.setAttribute("scope", "row");
+  title.textContent = book.title;
+  author.textContent = book.author;
+  numberOfPages.textContent = book.numberOfPages;
+  isRead.textContent = book.isRead;
+  tableRow.appendChild(title);
+  tableRow.appendChild(author);
+  tableRow.appendChild(numberOfPages);
+  tableRow.appendChild(isRead);
+  return tableRow;
+}
+
+const library = document.querySelector(".library");
+function displayBooks(books) {
+  for (let book of books) {
+    const tableRow = createABookTableRow(book);
+    library.appendChild(tableRow);
+  }
+}
+
+displayBooks(myLibrary);
+function addABookTableRow(book) {
+  const tableRow = createABookTableRow(book);
+  library.appendChild(tableRow);
+}
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = form.elements["title"].value;
+  const author = form.elements["author"].value;
+  const numberOfPages = form.elements["numberOfPages"].value;
+  const isRead = form.elements["isRead"].value;
+  addBookToLibrary(title, author, numberOfPages, isRead);
+  formControl.classList.toggle("hidden");
+});
+
+const newBtn = document.querySelector("#newBookForm");
+const formControl = document.querySelector(".formControl");
+const cancelForm = document.querySelector(".cancelForm");
+
+newBtn.addEventListener("click", (e) => {
+  formControl.classList.toggle("hidden");
+});
+
+cancelForm.addEventListener("click", (e) => {
+  formControl.classList.toggle("hidden");
+});
