1389f383d4016f13cfa2cf874ae9fbbadc9733c8
Thein Than Hein <thein.than.hein.th@gmail.com>
2022-01-01 20:44:47 -0800

    finish library

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let myLibrary = [
+const myLibrary = [
   { title: "The Hobbit", author: "Tjj", numberOfPages: 453, isRead: "No" },
   {
     title: "Lord of the Rings",
@@ -8,77 +8,110 @@ let myLibrary = [
   },
 ];
 
+// ** JS Functionality ** //
+
 function Book(title, author, numberOfPages, isRead) {
   this.title = title;
   this.author = author;
   this.numberOfPages = numberOfPages;
   this.isRead = isRead;
-  this.info = () => {
-    return `${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.isRead}`;
-  };
 }
 
-function addBookToLibrary(title, author, numberOfPages, isRead) {
-  const book = new Book(title, author, numberOfPages, isRead);
-  myLibrary.push(book);
-  addABookTableRow(book);
+function createBook(title, author, numberOfPages, isRead) {
+  return new Book(title, author, numberOfPages, isRead);
 }
 
-function createABookTableRow(book) {
-  const tableRow = document.createElement("tr");
+function addToLibrary(book, library) {
+  return library.push(book);
+}
+
+function deleteBook(bookIndex, library) {
+  return library.splice(bookIndex, 1);
+}
+
+// ** DOM Manipulation ** //
+
+const library = document.querySelector(".library");
+const formContainer = document.querySelector(".form-container");
+const newFormBtn = document.querySelector("#new-form-btn");
+const cancelFormBtn = document.querySelector("#cancel-form-btn");
+const newForm = document.querySelector("#new-form");
+
+newFormBtn.addEventListener("click", (e) => {
+  formContainer.classList.toggle("hidden");
+});
+
+cancelFormBtn.addEventListener("click", (e) => {
+  formContainer.classList.toggle("hidden");
+});
+
+newForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = newForm.elements["title"].value;
+  const author = newForm.elements["author"].value;
+  const numberOfPages = newForm.elements["numberOfPages"].value;
+  const isRead = newForm.elements["isRead"].value;
+  const book = createBook(title, author, numberOfPages, isRead);
+  addToLibrary(book, myLibrary);
+  createBookRow(book);
+  addBookRow(book);
+  formContainer.classList.toggle("hidden");
+});
+
+function delBookRow(bookIndex) {
+  library.childNodes.forEach((bookRow) => {
+    if (bookRow.dataset.key === bookIndex) {
+      return bookRow.remove();
+    }
+  });
+}
+
+function createDelBookBtn(library) {
+  const deleteBookBtn = document.createElement("button");
+  deleteBookBtn.textContent = "DEL";
+  deleteBookBtn.classList.add("del-book-btn");
+  deleteBookBtn.dataset.key = library.length - 1;
+  deleteBookBtn.addEventListener("click", (e) => {
+    const bookIndex = e.target.dataset.key;
+    deleteBook(bookIndex, myLibrary);
+    delBookRow(bookIndex);
+  });
+  return deleteBookBtn;
+}
+
+function createBookRow(book) {
+  const bookRow = document.createElement("tr");
   const title = document.createElement("th");
   const author = document.createElement("td");
   const numberOfPages = document.createElement("td");
   const isRead = document.createElement("td");
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "x";
-  deleteBtn.classList.add("del-btn");
+  const delBookContainer = document.createElement("td");
+  const deleteBookBtn = createDelBookBtn(myLibrary);
+  delBookContainer.appendChild(deleteBookBtn);
+  bookRow.dataset.key = myLibrary.length - 1;
   title.setAttribute("scope", "row");
   title.textContent = book.title;
   author.textContent = book.author;
   numberOfPages.textContent = book.numberOfPages;
   isRead.textContent = book.isRead;
-  tableRow.appendChild(title);
-  tableRow.appendChild(author);
-  tableRow.appendChild(numberOfPages);
-  tableRow.appendChild(isRead);
-  tableRow.appendChild(deleteBtn);
-  return tableRow;
+  bookRow.appendChild(title);
+  bookRow.appendChild(author);
+  bookRow.appendChild(numberOfPages);
+  bookRow.appendChild(isRead);
+  bookRow.appendChild(delBookContainer);
+  return bookRow;
 }
 
-const library = document.querySelector(".library");
+function addBookRow(book) {
+  const bookRow = createBookRow(book);
+  library.appendChild(bookRow);
+}
+
 function displayBooks(books) {
   for (let book of books) {
-    const tableRow = createABookTableRow(book);
-    library.appendChild(tableRow);
+    const bookRow = createBookRow(book);
+    library.appendChild(bookRow);
   }
 }
 
 displayBooks(myLibrary);
-function addABookTableRow(book) {
-  const tableRow = createABookTableRow(book);
-  library.appendChild(tableRow);
-}
-
-const form = document.querySelector("#form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const title = form.elements["title"].value;
-  const author = form.elements["author"].value;
-  const numberOfPages = form.elements["numberOfPages"].value;
-  const isRead = form.elements["isRead"].value;
-  addBookToLibrary(title, author, numberOfPages, isRead);
-  formControl.classList.toggle("hidden");
-});
-
-const newBtn = document.querySelector("#newBookForm");
-const formControl = document.querySelector(".formControl");
-const cancelForm = document.querySelector(".cancelForm");
-
-newBtn.addEventListener("click", (e) => {
-  formControl.classList.toggle("hidden");
-});
-
-cancelForm.addEventListener("click", (e) => {
-  formControl.classList.toggle("hidden");
-});
